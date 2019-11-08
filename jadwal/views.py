172b9753c2c4c81261e6bc988b6f6828bc67f13c
acroziqin <achmadchoirurroziqin@gmail.com>
2019-11-08 17:14:28 +0700

    UI selesai. Selanjutnya terapkan Django

---
+++ jadwal/views.py
@@ -0,0 +1,13 @@
+from django.views.generic.base import TemplateView
+
+class IndexView(TemplateView):
+    pass
+
+class TentukanView(TemplateView):
+    pass
+
+class ImportExcelView(TemplateView):
+    pass
+
+class HasilView(TemplateView):
+    pass
